8d49f7ee7f0d775605e9ac2073c0858efc341289
MC派大星 <60082801+trainKing-star@users.noreply.github.com>
2020-05-03 17:12:29 +0800

    23

--- try/My_think/app.py
+++ try/My_think/app.py
@@ -5,11 +5,9 @@ app = Flask(__name__)
 
 @app.route('/',methods=['POST'])
 def hello_world():
-    file=request.files['file']
-    file = file.read()
-    file = base64.b64encode(file)
-    file = str(file, 'utf-8')
-    return jsonify({'photo':file})
+    data=request.form.get('data')
+    print(data)
+    return jsonify({'AAB':'true'})
 
 if __name__ == '__main__':
     app.run()
